fdb4982b68c8f6400e9e66e31d94cd41518a0ee9
unknown <kaushalsorathiya777@gmail.com>
2024-01-12 22:32:42 +0530

    added hashset questions

---
+++ JavaInterviewQuestions/src/HashSet/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+	public static boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<Integer>();
+		for(int num : nums) {
+			if(set.contains(num)) {
+				return true;
+			}
+			set.add(num);
+		}
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,1};
+		System.out.println(containsDuplicate(nums));
+	}
+
+}
