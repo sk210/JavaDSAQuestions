9ca3b83e2782616372a4fd3d9ce641ed2b55275a
unknown <kaushalsorathiya777@gmail.com>
2024-03-09 22:19:40 +0530

    added sliding window anwser 2

--- JavaInterviewQuestions/src/SlidingWindow/MaximumAverageSubarray1Ans2.java
+++ JavaInterviewQuestions/src/SlidingWindow/MaximumAverageSubarray1Ans2.java
@@ -1,25 +1,29 @@
 package SlidingWindow;
 
-public class MaximumAverageSubarray1 {
+public class MaximumAverageSubarray1Ans2 {
 
 	public static double findMaxAverage(int[] nums, int k) {
 		double maxSum = Integer.MIN_VALUE;
 		double tempSum = 0;
-
-		for (int i = 0; i < nums.length; i++) {
-			tempSum += nums[i];
-			if (i >= k - 1) {
+		int i = 0;
+		int j = 0;
+		
+		while(j < nums.length) {
+			tempSum += nums[j];
+			if(j-i+1 == k) {
 				maxSum = Math.max(maxSum, tempSum);
-				tempSum -= nums[i - (k - 1)];
+				tempSum -= nums[i]; 
+				i++;
 			}
+			j++;
 		}
-
 		return maxSum / k;
 	}
 
 	public static void main(String[] args) {
 		int[] nums = { 1,12,-5,-6,50,3 };
 		System.out.println(findMaxAverage(nums, 4));
+
 	}
 
 }
